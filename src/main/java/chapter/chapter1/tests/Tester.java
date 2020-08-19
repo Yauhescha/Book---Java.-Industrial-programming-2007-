bc74b2dc08d776bde111c91652fec45716abddfc
Dell <Dell@DESKTOP-RF39OKA>
2020-08-19 23:16:04 +0300

    файл для тестовых заданий

---
+++ src/main/java/chapter/chapter1/tests/Tester.java
@@ -0,0 +1,15 @@
+package main.java.chapter.chapter1.tests;
+
+public class Tester {
+	public static void main() {
+		System.out.print("А");
+	}
+
+	public static void main(String args) {
+		System.out.print("Б");
+	}
+
+	public static void main(String[] args) {
+		System.out.print("В");
+	}
+}
