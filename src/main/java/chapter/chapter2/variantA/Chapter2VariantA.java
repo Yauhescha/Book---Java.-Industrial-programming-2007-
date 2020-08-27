445c6ec2fe3e332d80d42d76b3b5e0e5f016a38b
Yauhescha <evgescha.maslov007@yandex.ru>
2020-08-27 15:21:57 +0300

    сделано 5 заданий

--- src/main/java/chapter/chapter2/variantA/Chapter2VariantA.java
+++ src/main/java/chapter/chapter2/variantA/Chapter2VariantA.java
@@ -1,25 +1,29 @@
 package main.java.chapter.chapter2.variantA;
 
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Chapter2VariantA {
 	Scanner sc = new Scanner(System.in);
 	int countLinesToInput;
 	String[] arrString;
+
 	public static void main(String[] args) {
 		Chapter2VariantA chapter2 = new Chapter2VariantA();
-		System.out.println(
-				"1. Ввести n строк с консоли, найти самую короткую и самую длинную строки. Вывести найденные строки и их длину.");
-		chapter2.task1();
-		System.out.println(
-				"2. Ввести n строк с консоли. Упорядочить и вывести строки в порядке возрастания (убывания) значений их длины.");
-		chapter2.task2();
-		System.out.println(
-				"3. Ввести n строк с консоли. Вывести на консоль те строки, длина кото-рых меньше (больше) средней, а также длину.");
-		chapter2.task3();
-		System.out.println(
-				"4. Ввести n слов с консоли. Найти слово, в котором число различных символов минимально. Если таких слов несколько, найти первое из них.");
-		chapter2.task4();
+//		System.out.println(
+//				"1. Ввести n строк с консоли, найти самую короткую и самую длинную строки. Вывести найденные строки и их длину.");
+//		chapter2.task1();
+//		System.out.println(
+//				"2. Ввести n строк с консоли. Упорядочить и вывести строки в порядке возрастания (убывания) значений их длины.");
+//		chapter2.task2();
+//		System.out.println(
+//				"3. Ввести n строк с консоли. Вывести на консоль те строки, длина кото-рых меньше (больше) средней, а также длину.");
+//		chapter2.task3();
+//		System.out.println(
+//				"4. Ввести n слов с консоли. Найти слово, в котором число различных символов минимально. Если таких слов несколько, найти первое из них.");
+//		chapter2.task4();
 		System.out.println(
 				"5. Ввести n слов с консоли. Найти количество слов,  содержащих только символы латинского алфавита, а среди них – количество слов с равным числом гласных и согласных букв.");
 		chapter2.task5();
@@ -53,24 +57,134 @@ public class Chapter2VariantA {
 
 	}
 
+	private void print(String[] obArr) {
+		for (int i = 0; i < obArr.length; i++) {
+			System.out.println(obArr[i]);
+		}
+	}
+
+	private String[] getNLinesFromConsole() {
+		System.out.println("Cколько слов/строк будет введено?");
+		int n = sc.nextInt();
+		sc.nextLine();
+		String arr[] = new String[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextLine();
+		}
+		return arr;
+	}
+
 //	1. Ввести n строк с консоли, найти самую короткую и самую длинную строки. Вывести найденные строки и их длину.
 	public void task1() {
+		String[] strings = getNLinesFromConsole();
+
+		int indexMin, indexMax;
+		indexMax = indexMin = 0;
+		for (int i = 0; i < strings.length; i++) {
+			if (strings[indexMin].length() > strings[i].length())
+				indexMin = i;
+			if (strings[indexMax].length() < strings[i].length())
+				indexMax = i;
+		}
+
+		System.out.println("Minimum and length");
+		System.out.println(strings[indexMin].length() + ": " + strings[indexMin]);
+		System.out.println("Maximum and length");
+		System.out.println(strings[indexMax].length() + ": " + strings[indexMax]);
 	}
 
 //	2. Ввести n строк с консоли. Упорядочить и вывести строки в порядке возрастания (убывания) значений их длины.
 	public void task2() {
+		String[] arr = getNLinesFromConsole();
+		String temp;
+		boolean isOkay = false;
+		while (!isOkay) {
+			isOkay = true;
+			for (int i = 0; i < arr.length - 1; i++) {
+				if (arr[i].length() > arr[i + 1].length()) {
+					temp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+					isOkay = false;
+				}
+			}
+		}
+
+		print(arr);
 	}
 
 //	3. Ввести n строк с консоли. Вывести на консоль те строки, длина кото-рых меньше (больше) средней, а также длину.
 	public void task3() {
+		String[] arr = getNLinesFromConsole();
+		int average = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			average += arr[i].length();
+		}
+		average /= arr.length;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].length() > average)
+				System.out.println(arr[i].length() + ": " + arr[i]);
+		}
 	}
 
 //	4. Ввести n слов с консоли. Найти слово, в котором число различных символов минимально. Если таких слов несколько, найти первое из них.
 	public void task4() {
+		String[] arr = getNLinesFromConsole();
+		HashMap<Byte, Integer> map;
+
+		int indexMin = 0, countCharMin;
+		
+		map = setByteCountInMap(arr[0]);
+		countCharMin = map.size();
+
+		for (int i = 1; i <arr.length; i++) {
+			map = setByteCountInMap(arr[i]);
+			if (countCharMin > map.size()) {
+				countCharMin = map.size();
+				indexMin = i;
+			}
+		}
+
+		System.out.println(countCharMin + ": " + arr[indexMin]);
+	}
+
+	private HashMap<Byte, Integer> setByteCountInMap(String str) {
+		HashMap<Byte, Integer> map = new HashMap<Byte, Integer>();
+		byte[] bytes = str.getBytes();
+		for (int j = 0; j < bytes.length; j++) {
+			map = addInMap(map, bytes[j]);
+		}
+		return map;
+	}
+
+	private HashMap<Byte, Integer> addInMap(HashMap<Byte, Integer> map, Byte bt) {
+		if (!map.containsKey(bt))
+			map.put(bt, 1);
+		else {
+			Integer integer = map.get(bt);
+			integer++;
+			map.put(bt, integer);
+		}
+		return map;
+
 	}
 
 //	5. Ввести n слов с консоли. Найти количество слов,  содержащих только символы латинского алфавита, а среди них – количество слов с равным числом гласных и согласных букв.
 	public void task5() {
+//		String[] arr = getNLinesFromConsole();
+//		
+//		Pattern pattern = Pattern.compile("[a-zA-Z\\d\\s\\p{Punct}]*");
+//		
+//		Matcher matcher;
+//		for (int i = 0; i < arr.length; i++) {
+//			matcher = pattern.matcher(arr[i]);
+//			if(matcher.matches()) {
+//				
+//			}System.out.println(arr[i]);
+//		}
+		
 	}
 
 //	6. Ввести n слов с консоли. Найти слово,  символы в котором идут в стро-гом порядке возрастания их кодов. Если таких слов несколько, найти первое из них.
